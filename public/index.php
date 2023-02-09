eb090c6544aa83c0868e7ee22bbcd159b9e0c230
Vladimir Oleynikov <v.oleynikov2000@gmail.com>
2023-02-09 23:29:27 +0300

    test

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Framework\Http\Message\ServerRequest;
 use Framework\Http\Message\ServerResponse;
 
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $request = ServerRequest::createFromGlobals();
